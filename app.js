62918ca8a3ef53814381aabc36afdbfb66740ee1
andresvalledelacruz <andresvalle@andresvalle.es>
2026-08-28 12:54:28 +0200

    feat: cargar pestaña Necesito Ayuda Urgente

--- app.js
+++ app.js
@@ -8,6 +8,7 @@
     document.body.append(script);
   };
   load('/app-core.js',()=>{
+    load('/urgent-help-nav.js');
     load('/next-step-guidance.js',()=>load('/next-step-adapter.js'));
   });
 })();
