72738eacacc6d625a44cb955cea6e06b581d68be
andresvalledelacruz <andresvalle@andresvalle.es>
2026-09-18 12:27:51 +0200

    Buscar Ayuda: vocabulario coloquial acotado con regresiones Safety (#277)

--- src/search-normalization.js
+++ src/search-normalization.js
@@ -1,13 +1,20 @@
 // Deliberately bounded corrections: never fuzzy-match short words or negation.
 const CORRECTIONS = Object.freeze({
   q: 'que', k: 'que', xq: 'porque', pq: 'porque',
-  travajo: 'trabajo', trabjo: 'trabajo', curro: 'trabajo',
+  travajo: 'trabajo', trabjo: 'trabajo', curro: 'trabajo', currelo: 'trabajo',
+  currar: 'trabajar', currando: 'trabajando', currela: 'trabajador',
+  coleguis: 'amigos', amiwos: 'amigos', hamigos: 'amigos',
+  agobiao: 'agobiado', quemao: 'quemado',
   alkiler: 'alquiler', alqiler: 'alquiler', arquiler: 'alquiler',
-  deudad: 'deudas', deudaz: 'deudas', despidido: 'despedido',
+  deudad: 'deudas', deudaz: 'deudas', despidido: 'despedido', despedio: 'despedido',
+  hipteca: 'hipoteca', ipoteca: 'hipoteca', prestmo: 'prestamo',
+  curriculun: 'curriculum', curriclum: 'curriculum',
   suicidame: 'suicidarme', sucidarme: 'suicidarme', suisidarme: 'suicidarme',
   sucidio: 'suicidio', suisidio: 'suicidio',
+  sucidarse: 'suicidarse', suisidarse: 'suicidarse',
+  sucido: 'suicido', suisido: 'suicido',
   maltratto: 'maltrato', maltratta: 'maltrata',
-  agression: 'agresion', agrecion: 'agresion'
+  agression: 'agresion', agrecion: 'agresion', agreison: 'agresion'
 });
 
 export function normalizeSearchText(value = '') {
